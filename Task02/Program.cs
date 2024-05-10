887f4a755b5f72af9317a1c651262a5a0944f1c9
Igor Korzhan <igor.human@gmail.com>
2024-05-10 21:43:58 +0300

    Добавьте файлы проекта.

---
+++ Task02/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using Task02;
+
+namespace task02
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int trainCount = 5;
+            Train[] trains = new Train[trainCount];
+
+            // Введення даних
+            for (int i = 0; i < trainCount; i++)
+            {
+                Console.WriteLine($"Введіть інформацію про поїзд {i + 1}:");
+                Console.Write("Пункт призначення: ");
+                string destination = Console.ReadLine();
+
+                Console.Write("Номер поїзду: ");
+                int trainNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("Час прибуття (гг:хх): ");
+                TimeSpan departureTime = TimeSpan.Parse(Console.ReadLine());
+
+                trains[i] = new Train(destination, trainNumber, departureTime);
+            }
+
+            // Сортування поїздів за номерами
+            Array.Sort(trains, (t1, t2) => t1.TrainNumber.CompareTo(t2.TrainNumber));
+
+            // Пошук поїзда за номером
+            Console.Write("Введіть номер потягу для пошуку: ");
+            int searchNumber = int.Parse(Console.ReadLine());
+
+            var foundTrain = trains.FirstOrDefault(t => t.TrainNumber == searchNumber);
+
+            if (foundTrain.TrainNumber != 0)
+            {
+                Console.WriteLine("Знайдений потяг: ");
+                Console.WriteLine(foundTrain);
+            }
+            else
+            {
+                Console.WriteLine("Не знайдено потяг за введеним номером.");
+            }
+
+            Console.WriteLine("Натисніть ентер для знищення...");
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Task02/Train.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    public struct Train
+    {
+        public string Destination { get; set; }
+        public int TrainNumber { get; set; }
+        public TimeSpan DepartureTime { get; set; }
+
+        public Train(string destination, int trainNumber, TimeSpan departureTime)
+        {
+            Destination = destination;
+            TrainNumber = trainNumber;
+            DepartureTime = departureTime;
+        }
+
+        public override string ToString()
+        {
+            return $"Поїзд №{TrainNumber} до {Destination} прибуває о {DepartureTime}";
+        }
+    }
+
+}
